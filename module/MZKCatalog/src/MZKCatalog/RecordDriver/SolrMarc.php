cb9dbc43e3056e053c5aae4b0392dac82f641897
Erich Duda <dudaerich@gmail.com>
2013-08-30 09:51:10 +0200

    Add EOD funcionality
    Conflicts:
    	module/MZKCatalog/src/MZKCatalog/RecordDriver/SolrMarc.php

---
+++ module/MZKCatalog/src/MZKCatalog/RecordDriver/SolrMarc.php
@@ -0,0 +1,31 @@
+<?php
+namespace MZKCatalog\RecordDriver;
+use VuFind\RecordDriver\SolrMarc As ParentSolrDefault;
+
+class SolrMarc extends ParentSolrDefault
+{   
+    
+    public function getBibinfoForObalkyKnih()
+    {
+        $bibinfo = array(
+            "authors" => array($this->getPrimaryAuthor()),
+            "title" => $this->getTitle(),
+        );
+        $isbn = $this->getCleanISBN();
+        if (!empty($isbn)) {
+            $bibinfo['isbn'] = $isbn;
+        }
+        $year = $this->getPublicationDates();
+        if (!empty($year)) {
+            $bibinfo['year'] = $year[0];
+        }
+        return $bibinfo;
+    }
+    
+    public function getEOD()
+    {
+    	$eod = $this->getFirstFieldValue('EOD', array('a'));
+    	return ($eod == 'Y')?true:false;
+    }
+
+}
